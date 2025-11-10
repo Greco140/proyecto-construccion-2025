c9413181c7fce59adacf8cdc523c2e1f5deff8c9
EmiCG <emicontreras250@gmail.com>
2025-11-09 18:25:34 -0600

    Inicializacion del proyecto con la configuracion inicial de dependencias

---
+++ src/main/java/com/example/DynaDocs/DynaDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DynaDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynaDocsApplication.class, args);
+	}
+
+}
